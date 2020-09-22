edac4e867ddd88ca0f01f9be9344aaba5242438b
pinomartin <martin91pino@gmail.com>
2020-09-22 20:15:45 -0300

    Slider Responsive Form

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@ const tl = gsap.timeline({defaults: {
     ease : "power1.out"
 }});
 
-tl.to('.text' , {y: '0%', duration : 1, stagger: 0.20});
+tl.to('.text' , {y: '0%', duration : 1.5, stagger: 0.20});
 tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
-tl.to('.intro', {y:'-100%', duration: 1}, "-=1");
+tl.to('.intro', {y:'-100%', duration: 1.2}, "-=1");
 
 tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
 tl.fromTo('.titulo', {opacity: 0}, {opacity: 1, duration: 1} , "-=1");
